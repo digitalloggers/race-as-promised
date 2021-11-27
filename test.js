c91dc7b38010772db6439da330c1bfb3ef8c92ca
Sergey Zakharchenko <szakharchenko@digital-loggers.com>
2021-11-27 23:12:51 +0000

    Make test output meaningful statements about Promise.race() implementations

--- test.js
+++ test.js
@@ -3,9 +3,12 @@
  * https://github.com/nodejs/node/issues/17469#issuecomment-685216777
  *
  * Adapted to module structure.
+ *
+ * Adjusted to run for a finite time and perform explicit leak checks.
  */
 
-const race = require("./"); // change to Promise.race to see the difference
+const raceAsPromised = require("./");
+const nativeRace = Promise.race.bind(Promise);
 
 async function randomString(length) {
   await new Promise((resolve) => setTimeout(resolve, 1));
@@ -17,18 +20,45 @@ async function randomString(length) {
   return result;
 }
 
-(async function main() {
-  let i = 0;
+const iterationCount = 1000;
+const stringSize = 10000;
+
+function usageMeaningfullyIncreasing(usages, key) {
+  return usages[2][key] - usages[0][key] > 2 * iterationCount * stringSize && usages[2][key] - usages[1][key] > (usages[1][key] - usages[0][key]) / 2
+}
+
+function detectLeak(usages) {
+  return usageMeaningfullyIncreasing(usages, 'rss') || usageMeaningfullyIncreasing(usages, 'heapUsed')
+}
+
+async function run(race) {
   const pending = new Promise(() => {});
-  while (true) {
+  for (let i = 0; i < iterationCount; i++) {
     // We use random strings to prevent string interning.
     // Pass a different length string to see effects on memory usage.
-    await race([pending, randomString(10000)]);
-    if (i++ % 1000 === 0) {
-      const usage = process.memoryUsage();
-      const rss = Math.round(usage.rss / (1024 ** 2) * 100) / 100;
-      const heapUsed = Math.round(usage.heapUsed / (1024 ** 2) * 100) / 100;
-      console.log(`RSS: ${rss} MiB, Heap Used: ${heapUsed} MiB`);
-    }
+    await race([pending, randomString(stringSize)]);
   }
+}
+
+async function test(label, race, expectPass) {
+  const usages = [];
+  usages.push(process.memoryUsage());
+  await run(race);
+  usages.push(process.memoryUsage());
+  await run(race);
+  usages.push(process.memoryUsage());
+  const pass = !detectLeak(usages)
+  const expectationMet = pass == expectPass
+  console.log(`${expectationMet ? "ok" : "ERROR"}: ${label} ${pass ? "passed" : "failed"} the memory leak test`);
+  return expectationMet
+}
+
+(async function main() {
+  // NB: we run the test not expected to leak first
+  process.exit(
+    await test('race-as-promised', raceAsPromised, true)
+    &&
+    await test('native Promise.race', nativeRace, false)
+    ? 0 : 1
+  );
 })();
